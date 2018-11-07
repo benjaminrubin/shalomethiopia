d9ffa70aff93fee5887d99e2b836572e8947ba40
Benjamin Rubin <bennyfrubin@gmail.com>
2018-11-07 12:27:46 -0800

    Major update to about ethiopia page

---
+++ scripts/about-ethiopia.js
@@ -0,0 +1,142 @@
+function sysout(something) {
+    console.log(something);
+}
+
+// Function to populate Ethiopia's current time
+$(document).ready(function() {
+    //Getting the time in Ethiopia
+    var date = new Date();
+    var period = "AM";
+    // Ethiopia is three hours ahead of UTC
+    var hr = (date.getUTCHours() + 3) % 12;
+    var min = date.getUTCMinutes();
+    if (hr > 12 && hr < 24) {
+        period = "PM";
+    }
+    if (min < 10) {
+        min = "0" + min;
+    }
+
+    $('#ethiopia-time').html(hr + "<span class=\"blink-me\">:</span>" + min + " " + period);
+
+
+    //Currency Conversion
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+
+            var USD = obj['results']['USD_ETB']['val'].toFixed(2);
+            var EUR = obj['results']['EUR_ETB']['val'].toFixed(2);
+
+            var currency = "ETB = Ethiopian Birr <br/> " +
+                "1 USD = " + USD + " ETB <br>" +
+                "1 EUR = " + EUR + " ETB";
+            $('#currency').html(currency);
+        }
+    };
+    xhttp.open("GET", "https://free.currencyconverterapi.com/api/v6/convert?q=USD_ETB,EUR_ETB", true);
+    xhttp.send();
+});
+
+
+var markers = [];
+var geocoder;
+var placeIds = {};
+
+
+
+
+
+
+//Code relating to Google Maps API
+function initMap() {
+    var mapOptions = {
+        zoom: 5.7,
+        //this removes unnecessary controls
+        streetViewControl: false,
+        fullscreenControl: false,
+        mapTypeControl: false,
+        // zoomControl: false,
+
+        //this is where the map is initially set to
+        center: {
+            lat: 10.100180,
+            lng: 38.915677
+        }
+
+    }
+    // Creating a new map
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    bounds = new google.maps.LatLngBounds();
+    geocoder = new google.maps.Geocoder();
+
+    placeIds['Addis Ababa'] = 'ChIJLUCr9c6FSxYRSU2iN7C2Z4Q';
+    placeIds['Awassa'] = 'ChIJK7hnoHNFsRcRFo_bhZkaLKg';
+    placeIds['Axum'] = 'ChIJ-2zCoL78axYR4awVoIPc0HQ';
+    placeIds['Bahir Dar'] = 'ChIJaYDXBzPSRBYR-N8y9jSxsAo';
+    placeIds['Erta Ale'] = 'ChIJa7-GnkJgFBYRGLm54re0yww';
+    placeIds['Danakil Depression'] = 'ChIJc9CLsBlAExYRT9bupG-JLhk';
+    placeIds['Lalibela Rock Churches'] = 'ChIJAy1Pge4-QRYRYkuugucUdPI';
+    placeIds['Gondar'] = 'ChIJ304kPYIoQxYRZYqKNUViggc';
+    placeIds['Key Afer'] = 'ChIJ_8Ne7AHxpBcRPJAUqaDsMlw';
+    placeIds['Omo National Park'] = 'ChIJBZ2PzN5vphcRmI4vuHhp9ZQ';
+    placeIds['Semien Mountains National Park'] = 'ChIJnfgWbdTBaRYRLWISc9-EAnc';
+
+
+    var placeNumber = 1;
+
+
+
+
+    for (var key in placeIds) {
+        // console.log(key + ": " + placeIds[key]);
+        geocoder.geocode({ 'placeId': placeIds[key] }, function(results, status) {
+            if (status === 'OK') {
+                if (results[0]) {
+                    // map.setZoom(11);
+                    // map.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        title: key,
+                        map: map,
+                        position: results[0].geometry.location,
+                        label: {
+                            text: placeNumber.toString(),
+                            color: "white"
+                        }
+                    });
+                    markers.push(marker);
+                    placeNumber++;
+                }
+            } else {
+                window.alert('Geocoder failed due to: ' + status);
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+//Function that clicking on a destination displays the destionation's content
+// while hiding the content of all other destinations. Additionally, the destination's
+// pin turns green, as well as its corresponding pin on the map
+
+$(function() {
+    $('.destination-container').click(function() {
+        //if the content is not hidden, hide it
+        if ($(this).find('.destination-content').is(":visible")) {
+            $(this).find('.destination-content').hide();
+        } else {
+            $('.destination-container').find('.pin').removeClass('selected');
+            $('.destination-container').find('.destination-content').hide();
+            $(this).find('.destination-content').show();
+
+            //center the map to the respective pin and slightly zoom in on it
+        }
+    });
+});
